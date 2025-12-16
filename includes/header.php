4e6fe9ef7fbae90441eb7189a099c7f8cbe91826
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-16 13:59:29 +0530

    first commit

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS</title>
+
+    <!-- Correct CSS link -->
+    <link rel="stylesheet" href="css/public.css" media="all">
+</head>
+
+<body>
+
+    <div id="header">
+        <h1>Widget Corp</h1>
+    </div>
+
+    <div id="main">

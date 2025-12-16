61c3b6b9c5da4c57182f1dea9477e93a9eb5306f
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-16 16:44:10 +0530

    Add subject and page selection handling in content.php
    Implemented logic to capture and display selected subject and page based on URL parameters. Updated navigation links to include dynamic URLs for subjects and pages, enhancing user experience and interactivity.

--- content.php
+++ content.php
@@ -1,5 +1,18 @@
 <?php require_once("includes/connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
+
+   
+   <?php
+   $sel_subj = "";
+   $sel_page = "";
+   
+   if (isset($_GET['subj'])) {
+       $sel_subj = $_GET['subj'];
+   } elseif (isset($_GET['page'])) {
+       $sel_page = $_GET['page'];
+   }
+   ?>
+
 <?php include("includes/header.php"); ?>
 
 <table id="structure">
@@ -12,13 +25,16 @@
 
                 // Use returned data
                 while ($subject = mysqli_fetch_assoc($subject_set)) {
-                    echo "<li>{$subject['menu_name']}</li>";
+                    echo "<li> <a href=\"content.php?subj=" .urlencode($subject["id"]) . 
+                    "\">
+                    {$subject['menu_name']}</a></li>";
 
                     $page_set = get_pages_for_subject($connection, $subject["id"]);
 
                     echo "<ul class=\"pages\">";
                     while ($page = mysqli_fetch_assoc($page_set)) {
-                        echo "<li>{$page['menu_name']}</li>";
+                        echo "<li><a href=\"content.php?page=" . urlencode($page["id"]) .
+                        "\">{$page['menu_name']}</a></li>";
                     }
                     echo "</ul>";
                 }
@@ -28,6 +44,9 @@
 
         <td id="page">
             <h2>Content Area</h2>
+            <?php echo $sel_subj; ?> <br />
+            <?php echo $sel_page; ?> <br />
+
         </td>
     </tr>
 </table>
